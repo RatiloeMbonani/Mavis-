ebaa3ec130bb55c386144d650e20c555918601c2
Bukho127 <Bukho.mabula06@gmail.com>
2026-07-27 22:57:20 +0200

    mongoDB connectedDB

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express')
+const { connectDB } = require("./Config/config")
+require("dotenv").config();
 
 const app = express()
 
-const PORT = 3000;
-const data={
+const PORT = process.env.PORT || 5000;
+const data = {
     status: "running"
 };
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.status(200).json(data)
 })
 
-app.listen(PORT, ()=>{
-    console.log(`the server is running on port ${PORT}`)
-})
+async function startServer() {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`the server is running on port ${PORT}`)
+    })
+}
+startServer();
