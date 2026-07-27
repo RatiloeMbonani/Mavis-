18dae0a63bd55dd069ec9a4f8a99730310ce5195
Bukho127 <Bukho.mabula06@gmail.com>
2026-07-27 22:44:56 +0200

    configured mongoDB

--- Config/config.js
+++ Config/config.js
@@ -1,1 +1,30 @@
-//this will contain database configurations
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+        return client;
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+        process.exit(1);
+    }
+}
+
+module.exports = {
+    connectDB,
+    client,
+};
