adfb854c63ac482bf5156f443f53b2a6c30a88d7
Bukho Mabhula <Bukho.mabula06@gmail.com>
2026-07-27 22:01:38 +0200

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,1 +1,1 @@
-//this will container business logic/ things such as writing and reading from the database
+//this will container business logic/ things such as writing and reading and error handling from the database
