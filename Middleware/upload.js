1a06d51dd14486945911f3f9a46994ae58d1ee56
Bukho127 <Bukho.mabula06@gmail.com>
2026-07-28 12:46:03 +0200

    updated multer

--- Middleware/upload.js
+++ Middleware/upload.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-// Ensure the uploads folder exists
 const uploadDir = path.join(__dirname, '..', 'uploads', 'cvs');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
@@ -13,14 +12,14 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    // e.g., 65f1a2b3c4d5e6f7-1706438231492.pdf
+    
     const uniqueSuffix = `${req.user.user_id}-${Date.now()}`;
     cb(null, `${uniqueSuffix}${path.extname(file.originalname)}`);
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'application/pdf') {
+  if (file.mimetype === 'application/pdf') { // this protects our application from python scripts
     cb(null, true);
   } else {
     cb(new Error('Only PDF files are allowed'), false);
